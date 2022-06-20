6c2f6256d6381debdd1156a9551bf9fd165f8b23
Annox <63057879+DylanBrss@users.noreply.github.com>
2022-06-20 16:27:31 +0200

    4-destructuring fini

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -1,13 +1,23 @@
 //4.1 Découverte
 
-const [a, b] = [1, 2, 3, 4]
+const [a, b, c, d] = [1, 2, 3, 4]
 console.log(a)
 console.log(b)
+console.log(c)
 
-const {first, last} = {first: 'Paul', last: 'Henta', age: 35}
+const {first, last, age} = {first: 'Paul', last: 'Henta', age: 35}
 console.log(first)
 console.log(last)
+console.log(age)
 
 //4.2 Application
 
 console.log(data)
+
+const dd = data.SEM_10427["dateDebut"];
+const df = data.SEM_10427["dateFin"];
+const t = data.SEM_10427["texte"];
+
+console.log(dd)
+console.log(df);
+console.log(t);
