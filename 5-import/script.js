037e040ef903447ddb5597fd886eefad73a2f4b0
Annox <63057879+DylanBrss@users.noreply.github.com>
2022-06-20 16:56:58 +0200

    5-import fini

--- 5-import/script.js
+++ 5-import/script.js
@@ -1,8 +1,24 @@
 //5-1 Import
 
-import {hello} from './functions.js'
+import {hello} from './functions.js';
+import {showError} from './functions.js';
+import y from './functions.js';
 
-hello()
+hello();
+showError();
+y();
 
 // 5-2 Export
 const [a, b] = [7, 9]
+
+import {sum} from './maths.js';
+import {sub} from './maths.js';
+import {multiply} from './maths.js';
+import {divide} from './maths.js';
+import {pow} from './maths.js';
+
+sum();
+sub();
+multiply();
+divide();
+pow();
